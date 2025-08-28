1f32cf94d2a8cb3314ffbe02850875b07c926571
abhijain05 <minajain65@gmail.com>
2025-08-28 10:06:51 +0000

    Update from NeuroTune - 2025-08-28T10:06:51.401Z

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,26 +1,37 @@
 sap.ui.define([
-  "sap/ui/core/UIComponent",
-  "sap/ui/Device"
-], function (UIComponent, Device) {
-  "use strict";
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device"
+], function(UIComponent, Device) {
+	"use strict";
 
-  return UIComponent.extend("converted.productsetupwizardview.Component", {
-    metadata: {
-      manifest: "json"
-    },
+	return UIComponent.extend("converted.productsetupwizardview.Component", {
+		metadata: {
+			manifest: "json"
+		},
 
-    /**
-     * The component is initialized by UI5 automatically during the startup of the app
-     */
-    init: function () {
-      // Call the base component's init function
-      UIComponent.prototype.init.apply(this, arguments);
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and sets up the models
+		 */
+		init: function() {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
 
-      // Set device model
-      this.setModel(new sap.ui.model.json.JSONModel(sap.ui.Device), "device");
+			// set the device model
+			this.setModel(new sap.ui.model.json.JSONModel({
+				isTouch: Device.support.touch,
+				isNoTouch: !Device.support.touch,
+				isDesktop: Device.system.desktop,
+				isNoDesktop: !Device.system.desktop,
+				isTablet: Device.system.tablet,
+				isNoTablet: !Device.system.tablet,
+				isPhone: Device.system.phone,
+				isNoPhone: !Device.system.phone,
+				listMode: sap.ui.Device.support.touch ? "None" : "SingleSelectMaster",
+				searchSupported: Device.support.search
+			}), "device");
 
-      // Initialize the router for navigation
-      this.getRouter().initialize();
-    }
-  });
+			// initialize the router
+			this.getRouter().initialize();
+		}
+	});
 });
